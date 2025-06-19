def119395a0b5dcaa9e0ab1a6805ffa2536607f6
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-19 16:51:20 -0500

    feat: implement data fetching and error handling in CheckMonitors component

--- src/app/(dashboard)/check-monitor/components/CheckMonitors.tsx
+++ src/app/(dashboard)/check-monitor/components/CheckMonitors.tsx
@@ -1,8 +1,36 @@
+'use client';
+
+import {useFetchChecks} from "@/hooks/Fetch";
+import {useQuery} from "@tanstack/react-query";
+import isError from "next/dist/lib/is-error";
+import {Alert, AlertDescription, AlertTitle, SkeletonTable} from "@/components/ui";
+import {Terminal} from "lucide-react";
+import {DataTable} from "@/components/DataTable";
+import {Columns} from "@/app/(dashboard)/check-monitor/components/Columns";
+
 export const CheckMonitors = () => {
+
+	const {data: checks, isLoading, isError} = useQuery({
+		queryKey: ['checks'],
+		queryFn: useFetchChecks,
+	});
+
 	return (
 		<div className="flex flex-col gap-4">
-			<h1 className="text-2xl font-bold">Check Monitors</h1>
-			<p>This is where your check monitor component will be rendered.</p>
+			<div className="flex items-center justify-between w-full">
+				<h1 className="text-2xl font-bold">Check Monitors</h1>
+			</div>
+			{isError && (
+				<Alert variant="destructive">
+					<Terminal/>
+					<AlertTitle>Heads up!</AlertTitle>
+					<AlertDescription>There was an error fetching the data. Please try again later.</AlertDescription>
+				</Alert>
+			)}
+
+
+			{!isError &&
+				(isLoading ? <SkeletonTable /> : checks && <DataTable data={checks} columns={Columns} />)}
 		</div>
 	);
 }
