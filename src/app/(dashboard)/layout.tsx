20c12025f6b2d773676a5b76351fbf1f1f92ff5e
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-19 13:30:27 -0500

    feat: refactor SidebarInset to include children as a prop

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -10,8 +10,8 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
     return (
         <SidebarProvider>
             <AppSidebar />
-            <SidebarInset>
-                <main className="flex flex-1 flex-col gap-4 p-4 pt-0">{children}</main>
+            <SidebarInset className="flex flex-1 flex-col gap-4 p-4 pt-0">
+                {children}
             </SidebarInset>
         </SidebarProvider>
     );
