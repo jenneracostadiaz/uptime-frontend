d2ce4fc38e7412192d9161ec45da775cf2435b4f
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-19 14:54:29 -0500

    feat: add ComponentProvider to ComponentPage for enhanced functionality

--- src/app/(dashboard)/components/page.tsx
+++ src/app/(dashboard)/components/page.tsx
@@ -1,9 +1,11 @@
 import HeaderComponents from "@/app/(dashboard)/components/components/HeaderComponents";
+import {ComponentProvider} from "@/app/(dashboard)/components/components/ComponentProvider";
 
 export default function ComponentPage() {
     return (
         <>
             <HeaderComponents />
+            <ComponentProvider />
         </>
     );
 }
