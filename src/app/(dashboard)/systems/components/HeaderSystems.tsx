324534f28ea5c4eecb24fe009dfe30543233fa64
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-19 11:29:16 -0500

    feat: add HeaderSystems component and integrate it into SystemsPage

---
+++ src/app/(dashboard)/systems/components/HeaderSystems.tsx
@@ -0,0 +1,44 @@
+import {
+	Breadcrumb,
+	BreadcrumbItem,
+	BreadcrumbLink,
+	BreadcrumbList, BreadcrumbPage,
+	BreadcrumbSeparator,
+	Separator,
+	SidebarTrigger
+} from "@/components/ui";
+import {ModeToggle} from "@/components/ModeToggle";
+
+export default function HeaderSystems() {
+	return (
+		<header className="flex h-16 shrink-0 justify-between items-center gap-2 transition-[width,height] ease-linear group-has-data-[collapsible=icon]/sidebar-wrapper:h-12">
+			<div className="flex-1 flex items-center gap-2">
+				<SidebarTrigger className="-ml-1" />
+				<Separator
+					orientation="vertical"
+					className="mr-2 data-[orientation=vertical]:h-4"
+				/>
+				<Breadcrumb>
+					<BreadcrumbList>
+						<BreadcrumbItem className="hidden md:block">
+							<BreadcrumbLink href="#">
+								Uptime
+							</BreadcrumbLink>
+						</BreadcrumbItem>
+						<BreadcrumbSeparator className="hidden md:block" />
+						<BreadcrumbItem>
+							<BreadcrumbPage>Systems</BreadcrumbPage>
+						</BreadcrumbItem>
+					</BreadcrumbList>
+				</Breadcrumb>
+			</div>
+			<div className="flex items-center gap-2">
+				<Separator
+					orientation="vertical"
+					className="mr-2 data-[orientation=vertical]:h-4"
+				/>
+				<ModeToggle />
+			</div>
+		</header>
+	);
+}
