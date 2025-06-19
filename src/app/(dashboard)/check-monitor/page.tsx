5f06e4a9523f99a1539a9b17abffc3bdd6d19efb
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-19 16:32:42 -0500

    feat: add maintenance page for check monitor

---
+++ src/app/(dashboard)/check-monitor/page.tsx
@@ -0,0 +1,9 @@
+export default function checkMonitorPage() {
+	return (
+		<div className="flex h-full w-full items-center justify-center">
+			<div className="text-2xl font-bold text-gray-700">
+				Monitor is currently under maintenance.
+			</div>
+		</div>
+	)
+}
