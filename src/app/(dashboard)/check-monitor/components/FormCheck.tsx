e43b4a8f560269897f2ac9b29db32f933664a442
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-19 20:34:17 -0500

    feat: add FormCheck component for handling check forms

---
+++ src/app/(dashboard)/check-monitor/components/FormCheck.tsx
@@ -0,0 +1,10 @@
+import type{Check} from "@/type/System";
+
+interface FormCheckProps {
+	check?: Check;
+	onSuccess?: () => void;
+}
+
+export const FormCheck = ({check, onSuccess}:FormCheckProps) => {
+
+}
