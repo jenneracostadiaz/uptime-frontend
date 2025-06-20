fee933ed50cd4308766afd9c0fab1e90075949e7
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-20 14:33:32 -0500

    feat: add Uptime Event link to the main page

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -29,6 +29,13 @@ export default async function Home() {
                         Check Monitor allows you to track and review the status of various system checks and components.
                     </p>
                 </a>
+                <a href="/uptime-event"
+                   className="block p-6 bg-white border border-gray-200 rounded-lg shadow-sm hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+                    <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Uptime Event</h5>
+                    <p className="mt-2 text-sm text-gray-600 dark:text-gray-300">
+                        Uptime Event provides insights into the uptime and performance of your systems and components.
+                    </p>
+                </a>
             </div>
         </>
     );
