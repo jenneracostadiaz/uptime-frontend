7a53f57c3e844147cd5e5c24549b4ddab9a346f4
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-20 15:11:02 -0500

    feat: implement UptimeEvents component with data fetching and error handling

--- src/app/(dashboard)/uptime-event/components/UptimeEvents.tsx
+++ src/app/(dashboard)/uptime-event/components/UptimeEvents.tsx
@@ -1,9 +1,37 @@
+'use client';
+
+import {useFetchUptimeEvents} from "@/hooks/Fetch";
+import { useQuery } from '@tanstack/react-query';
+import {Alert, AlertDescription, AlertTitle, SkeletonTable} from "@/components/ui";
+import {Terminal} from "lucide-react";
+import {DataTable} from "@/components/DataTable";
+import {Columns} from "@/app/(dashboard)/uptime-event/components/Columns";
+
 export const UptimeEvents = () => {
+	const  {
+		data: uptimeEvents,
+		isLoading,
+		isError,
+	} = useQuery({
+		queryKey: ['uptimeEvents'],
+		queryFn: useFetchUptimeEvents,
+	});
+
 	return (
 		<div className="flex flex-col gap-4">
-			<h1 className="text-2xl font-bold">Uptime Events</h1>
-			<p className="text-gray-600">This page is under construction.</p>
-			<p className="text-gray-600">Please check back later.</p>
+			<div className="flex items-center justify-between w-full">
+				<h1 className="text-2xl font-bold">Uptime Events</h1>
+			</div>
+
+			{isError && (
+				<Alert variant="destructive">
+					<Terminal />
+					<AlertTitle>Heads up!</AlertTitle>
+					<AlertDescription>There was an error fetching the data. Please try again later.</AlertDescription>
+				</Alert>
+			)}
+
+			{!isError && (isLoading ? <SkeletonTable /> : uptimeEvents && <DataTable columns={Columns} data={uptimeEvents} />)}
 		</div>
 	);
 }
