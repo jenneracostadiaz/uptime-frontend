4cf329cac30e8735e59bda6b0a94ad89e9a6f939
C. Jenner Acosta Díaz <dev@jenner.pe>
2025-06-20 13:23:08 -0500

    feat: add Components_API_URL constant for component API endpoint

--- src/conts/conts.ts
+++ src/conts/conts.ts
@@ -1,1 +1,2 @@
-export const SystemAPI_URL = `${process.env.NEXT_PUBLIC_API_URL}/ServiceSystem`;
+export const Systems_API_URL = `${process.env.NEXT_PUBLIC_API_URL}/ServiceSystem`;
+export const Components_API_URL = `${process.env.NEXT_PUBLIC_API_URL}/Component`;
